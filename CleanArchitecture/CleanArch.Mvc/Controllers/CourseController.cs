8897ff6c862cd42f7c84b6311746c8ed0bb61f87
Mayur Phase <mayur.phase@silicus.com>
2020-11-02 23:56:46 +0530

    commit changes for course controller

--- CleanArchitecture/CleanArch.Mvc/Controllers/CourseController.cs
+++ CleanArchitecture/CleanArch.Mvc/Controllers/CourseController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Mvc.Controllers
 {
+    [Authorize]
     public class CourseController : Controller
     {
         private readonly ICourseService _courseService;
@@ -19,6 +21,7 @@ namespace CleanArch.Mvc.Controllers
         public IActionResult Index()
         {
             CourseViewModel model = _courseService.GetCourses();
+
             return View(model);
         }
     }
